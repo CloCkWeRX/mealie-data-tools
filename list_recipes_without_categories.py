771eb848aa6506198cee69562cebbdb09fff8f02
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-10-19 18:24:19 +1030

    Fix the mealie-client pagination

--- list_recipes_without_categories.py
+++ list_recipes_without_categories.py
@@ -19,16 +19,19 @@ async def main():
     async with MealieClient(base_url=BASE_URL, api_token=API_TOKEN) as client:
         print("Fetching recipes from Mealie…")
 
-        all_recipes = await client.recipes.get_all(per_page=1000)
-        print(f"Found {len(all_recipes)} recipes.\n")
+        page = 1
+        while page == 1 or len(all_recipes) == 100:
+            all_recipes = await client.recipes.get_all(per_page=100, page=page)
+            page = page + 1
+            print(f"Found {len(all_recipes)} recipes.\n")
 
-        for recipe_summary in all_recipes:
-            recipe = await client.recipes.get(recipe_summary.id)
+            for recipe_summary in all_recipes:
+                recipe = await client.recipes.get(recipe_summary.id)
 
-            if len(recipe.recipe_category) == 0:
-                print(
-                    f"{recipe.name} ({BASE_URL}/g/home/r/{recipe.slug}) has no categories"
-                )
+                if len(recipe.recipe_category) == 0:
+                    print(
+                        f"{recipe.name} ({BASE_URL}/g/home/r/{recipe.slug}) has no categories"
+                    )
 
 
 if __name__ == "__main__":
